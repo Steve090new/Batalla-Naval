914d5f87d24df8995bc69b49e82eb193fd2f1102
Steve090new <ethanbonilla450@gmail.com>
2025-11-20 15:00:43 -0300

    generador del tablero

--- Pruevas.java
+++ Pruevas.java
@@ -1,22 +1,17 @@
 public class Pruevas{
     public static void main(String[] args) {
-        int filas = 1;
-        int columnas = 10;
-        String array[][] =new String[columnas][filas];
-        for(int x=0;x<columnas;x++){
-            for(int y=0;y<filas;y++){
-                array[x][y] = "["+x+"]columna:";
+        int filas_y = 10;
+        int filas_x = 10;
+        String numeros = "   [1][2][3][4][5][6][7][8][9][10]\n";
+        String letras = "ABCDEFGHIJ";
+        String tablero = "";
+        for(int x=0;x<filas_x;x++){
+            tablero+="["+letras.charAt(x)+"]";
+            for(int y=0;y<filas_y;y++){
+                tablero+="[0]";
             }
+            tablero+="|\n";
         }
-        for(int x2=0;x2<columnas;x2++){
-            for(int y2=0;y2<filas;y2++){
-                array[x2][y2] += "filas";
-            }
-        }
-        for(int fila=0;fila<columnas;fila++){
-            for(int columna=0;columna<filas;columna++){
-                System.out.println(array[fila][columna]);
-            }
-        }
+        System.out.println(numeros+tablero);
     }
 }
